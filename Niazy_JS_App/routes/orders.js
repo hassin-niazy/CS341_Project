8e65d99e17831e05c7ee225aa69a8e23f3e93e28
Hassinullah Niazy <niazy27@up.edu>
2026-02-11 19:28:45 -0800

    Part 1

--- Niazy_JS_App/routes/orders.js
+++ Niazy_JS_App/routes/orders.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET orders listing. */
 router.get('/', function(req, res, next) {
-  res.send('Orders page');
+  res.json([{
+                "topping": "cherry",
+                "quantity": "28",
+            },
+            {   
+                "topping": "plain",
+                "quantity": "53",
+            },
+            {
+                "topping": "chocolate",
+                "quantity": "22",
+            }
+        
+            ]);
 });
 
 module.exports = router;
